c149b926b0eefaec169ac5abdeb5d5b92005031c
Naeem Iqbal <naeemiqbal996@gmail.com>
2023-09-26 15:10:18 +0200

    Upgraded Triton image to 22.12 version with pointcloud support

---
+++ docker/server_3d/install_torch_2.sh
@@ -0,0 +1,3 @@
+python3 -m pip install --upgrade pip
+python3 -m pip install --force torch==1.13.1+cu117 torchvision==0.14.1+cu117 --extra-index-url https://download.pytorch.org/whl/cu117
+# python3 -m pip install --force torch==2.0.0+cu117 torchvision==0.15.1+cu117 --index-url https://download.pytorch.org/whl/cu117
