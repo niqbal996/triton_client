f44e25c020d5f1226bc2f8e8b7caa4674d4ef6d0
Naeem Iqbal <naeem.iqbal@dfki.de>
2025-02-03 19:03:30 +0100

    Cleaned up, directly compile

--- docker/server_3d/install_spconv.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-python3 -m pip install --upgrade pip
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb && \
-dpkg -i cuda-keyring_1.0-1_all.deb && \ # download and install latest cuda keyrings
-apt-key del 7fa2af80 && \# remove outdated keys
-apt-get update
-apt-get install -y libboost-all-dev libgl1 # install boost
-pip install -r requirements.txt # install python dependencies 
